c11d4e90f973af30e3a274940ad5a3c6ff171c0c
Damian <58891112+damianroiz@users.noreply.github.com>
2022-07-26 17:01:43 -0400

    installing express

--- javascripts/script.js
+++ javascripts/script.js
@@ -91,6 +91,11 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score, 
+  });
 }
 
 // Adjust Ball Movement
@@ -101,7 +106,15 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+  socket.emit('ballMove', {
+    ballX, 
+    ballY,
+    score, 
+  });
 }
+
+
+
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
 function ballBoundaries() {
@@ -157,10 +170,11 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  computerAI();
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
@@ -183,6 +197,9 @@ function startGame() {
       if (paddleX[paddleIndex] > (width - paddleWidth)) {
         paddleX[paddleIndex] = width - paddleWidth;
       }
+      socket.emit('paddleMove', {
+        xPosition: paddleX[paddleIndex]
+      });
       // Hide Cursor
       canvas.style.cursor = 'none';
     });
@@ -201,9 +218,17 @@ socket.on('startGame', (refereeId) => {
   console.log('Referee is', refereeId);
 
   isReferee = socket.id === refereeId;
-  //startGame();
-})
+  startGame();
+});
 
+socket.on('paddleMove', (paddleMove) => {
+  const opponentPaddleIndex = 1- paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition; 
+});
+
+socket.on('ballMove', (ballData) => {
+  ({ballX, ballY, score} = ballData); 
+ });
 
 
 
--- server.js
+++ server.js
@@ -11,21 +11,11 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on ${PORT}...`)
 
+sockets.listen(io);
+
 let readyPlayerCount = 0;
 
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
 
-    socket.on('ready', () => {
-        console.log('Player ready', socket.id);
-
-        readyPlayerCount++;
-
-        if(readyPlayerCount === 2) {
-            io.emit('startGame', socket.id);
-        }
-    });
-});
 
 
 
